19ba489748580aa9fbbc29d14122436886bacb2b
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-26 18:55:16 -0300

    Two additional queries to test joins

--- myfinance-web-wo-top-level-statement/Scripts/Queries.sql
+++ myfinance-web-wo-top-level-statement/Scripts/Queries.sql
@@ -15,3 +15,18 @@ SELECT AVG(Valor) FROM Transacao WHERE TIPO = 'D'
 SELECT AVG(Valor) FROM Transacao WHERE TIPO = 'R'
 
 SELECT * FROM Transacao WHERE Data >= '20240101 00:00:00' AND Data <= '20241031 23:59:59'
+
+SELECT
+    T.*,
+    P.Nome As Categoria
+  FROM Transacao AS T
+  INNER JOIN PlanoConta AS P ON P.Id = T.PlanoContaId
+
+-- Como descobrir quais categorias de plano de contas ainda não possuem transações vinculadas
+SELECT
+    P.Id,
+    P.Nome,
+    T.Id AS CodigoTransacao
+  FROM PlanoConta AS P
+  LEFT JOIN Transacao AS T ON T.PlanoContaId = P.Id
+  WHERE T.Id IS NULL
