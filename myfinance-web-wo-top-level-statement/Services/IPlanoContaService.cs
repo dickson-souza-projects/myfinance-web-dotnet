e33515fa0a87353539d70428b6e66e0bf3764554
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-26 21:27:55 -0300

    Scaffolding service class and interface

---
+++ myfinance-web-wo-top-level-statement/Services/IPlanoContaService.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using myfinance.web.Domain;
+
+namespace myfinance.web.Services;
+
+public interface IPlanoContaService
+{
+    List<PlanoConta> ListarRegistros();
+
+    void Salvar(PlanoConta item);
+
+    void Excluir(int id);
+
+    PlanoConta RetornarRegistro(int id);
+}
---
+++ myfinance-web-wo-top-level-statement/Services/PlanoContaService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+using myfinance.web.Domain;
+
+namespace myfinance.web.Services;
+
+public class PlanoContaService : IPlanoContaService
+{
+    public void Excluir(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<PlanoConta> ListarRegistros()
+    {
+        throw new NotImplementedException();
+    }
+
+    public PlanoConta RetornarRegistro(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Salvar(PlanoConta item)
+    {
+        throw new NotImplementedException();
+    }
+}
