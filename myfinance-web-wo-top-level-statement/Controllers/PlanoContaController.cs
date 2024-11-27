3dd0c1ca74b60db4faad03dadd2fd69fd34d725a
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-26 21:05:48 -0300

    Basic scaffolding of PlanoConta controller and view

---
+++ myfinance-web-wo-top-level-statement/Controllers/PlanoContaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace myfinance.web.Controllers;
+
+public class PlanoContaController : Controller
+{
+    public PlanoContaController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        var resultView = View();
+        return resultView;
+    }
+}
