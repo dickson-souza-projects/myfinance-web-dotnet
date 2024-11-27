ff19ec2007d6383adc60f0189d343e75ff2d40a1
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2024-11-26 21:48:15 -0300

    Configured PlanoConta controller, service and view

--- myfinance-web-wo-top-level-statement/Controllers/PlanoContaController.cs
+++ myfinance-web-wo-top-level-statement/Controllers/PlanoContaController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using myfinance.web.Services;
+
 namespace myfinance.web.Controllers;
 
 public class PlanoContaController : Controller
 {
-    public PlanoContaController()
+    private readonly IPlanoContaService planoContaService;
+
+    public PlanoContaController(IPlanoContaService planoContaService)
     {
+        this.planoContaService = planoContaService;
     }
 
     public IActionResult Index()
     {
         var resultView = View();
+        ViewBag.List = planoContaService.ListarRegistros();
         return resultView;
     }
 }
--- myfinance-web-wo-top-level-statement/Services/PlanoContaService.cs
+++ myfinance-web-wo-top-level-statement/Services/PlanoContaService.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
+using Microsoft.Extensions.Logging;
+
+using myfinance.web.Controllers;
 using myfinance.web.Domain;
+using myfinance.web.Infrastructure;
 
 namespace myfinance.web.Services;
 
 public class PlanoContaService : IPlanoContaService
 {
+    private MyFinanceDbContext myFinanceDbContext;
+
+    public PlanoContaService(
+        ILogger<HomeController> logger,
+        MyFinanceDbContext myFinanceDbContext
+    )
+    {
+        this.myFinanceDbContext = myFinanceDbContext;
+    }
+
     public void Excluir(int id)
     {
         throw new NotImplementedException();
@@ -14,7 +29,8 @@ public class PlanoContaService : IPlanoContaService
 
     public List<PlanoConta> ListarRegistros()
     {
-        throw new NotImplementedException();
+        var list = myFinanceDbContext.PlanoConta.ToList();
+        return list;
     }
 
     public PlanoConta RetornarRegistro(int id)
